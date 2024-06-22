7339fcf9a809a1e65fd85d22d3e38f1ca487e422
Dariellysantos <daryh_cristina@hotmail.com>
2024-06-22 17:28:21 -0300

    feat: schema bd configured

---
+++ src/models/userSchema.js
@@ -0,0 +1,48 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  id: mongoose.Schema.Types.ObjectId,
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  Birthday: {
+    type: String,
+    required: true,
+  },
+  state: {
+    type: String,
+    required: true,
+  },
+  city: {
+    type: String,
+    required: true,
+  },
+  country: {
+    type: Number,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: new Date(),
+  },
+  updatedAt: {
+    type: Date,
+    default: new Date(),
+  },
+  deletedAt: {
+    type: Date,
+    default: new Date(),
+  },
+});
+
+module.exports = mongoose.model("user", userSchema);
