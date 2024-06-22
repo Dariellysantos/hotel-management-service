8ed7c97f0abffd3e34dd566a44656fcf1bc708e7
Dariellysantos <daryh_cristina@hotmail.com>
2024-06-22 19:59:01 -0300

    fead: route delete user

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -3,57 +3,83 @@ const UserSchema = require("../models/userSchema");
 
 const createUser = async (req, res) => {
     try {
-      const newUser = new UserSchema({
-        _id: new mongoose.Types.ObjectId(),
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password,
-        birthday: req.body.birthday,
-        state: req.body.state,
-        city: req.body.city,
-        country: req.body.country,
-      });
-  
-      const sevedUser = await newUser.save();
-  
-      res.status(200).json({
-        message: "user successfully registered!",
-        sevedUser,
-      });
-    } catch (error) {
-      res.status(500).json({
-        message: error.message,
-      });
-    }
-  };
+        const newUser = new UserSchema({
+            _id: new mongoose.Types.ObjectId(),
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password,
+            birthday: req.body.birthday,
+            state: req.body.state,
+            city: req.body.city,
+            country: req.body.country,
+        });
 
-  const getAll = async (req, res) => {
-    try {
-      const users = await UserSchema.find();
-      res.status(200).json(users);
-    } catch (error) {
-      res.status(500).json({
-        message: error.message,
-      });
-    }
-  };
+        const sevedUser = await newUser.save();
 
-  const getById = async (req, res) => {
-    try {
-      const user = req.params.id;
-      let found = await UserSchema.findById(user);
-  
-      res.status(200).json(found);
+        res.status(200).json({
+            message: "user successfully registered!",
+            sevedUser,
+        });
     } catch (error) {
-      if (user === undefined)
         res.status(500).json({
-          message: error.message,
+            message: error.message,
         });
     }
-  };
-  
+};
+
+const getAll = async (req, res) => {
+    try {
+        const users = await UserSchema.find();
+        res.status(200).json(users);
+    } catch (error) {
+        res.status(500).json({
+            message: error.message,
+        });
+    }
+};
+
+const getById = async (req, res) => {
+    try {
+        const user = req.params.id;
+        let found = await UserSchema.findById(user);
+
+        res.status(200).json(found);
+    } catch (error) {
+        if (user === undefined)
+            res.status(500).json({
+                message: error.message,
+            });
+    }
+};
+
+const deleteById = async (req, res) => {
+    try {
+        const user = req.params.id;
+        let found = await UserSchema.findOneAndDelete(user);
+
+        if (found === null) {
+            res.status(404).json({
+                message: "post not found.",
+                code: "NOT_FOUND_ERROR",
+            });
+        }
+
+        return res.status(200).json({
+            message: "Post deleted successfully!",
+            code: "SUCCESS",
+        });
+
+    } catch (error) {
+        return res.status(500).json({
+            message: "Internal error.",
+            code: "INTERNAL_SERVER_ERROR",
+        });
+    }
+};
+
 module.exports = {
     createUser,
     getAll,
-    getById
+    getById,
+    deleteById
 };
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -6,6 +6,7 @@ const controller = require("../controllers/userController");
 router.post("/user", controller.createUser);
 router.get("/user", controller.getAll);
 router.get("/user/:id", controller.getById);
+router.delete("/user/:id", controller.deleteById);
 
 
 module.exports = router;
