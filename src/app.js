f2a53516a2fcd7a1fe54b12ddc405ef53badc562
Dariellysantos <daryh_cristina@hotmail.com>
2024-06-22 18:25:28 -0300

    feat: endpoint create user

--- src/app.js
+++ src/app.js
@@ -1,10 +1,17 @@
 
+const express = require("express");
+const cors = require("cors");
+
 require("dotenv").config();
 const db = require("./database/mongoConfig");
 
+const userRoutes = require("./routes/userRoutes");
 
-const express = require("express");
 const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use("/hotelManagement", userRoutes);
 
 db.connect();
 
---
+++ src/controllers/userController.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+const UserSchema = require("../models/userSchema");
+
+const createUser = async (req, res) => {
+    try {
+      const newUser = new UserSchema({
+        _id: new mongoose.Types.ObjectId(),
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+        birthday: req.body.birthday,
+        state: req.body.state,
+        city: req.body.city,
+        country: req.body.country,
+      });
+  
+      const sevedUser = await newUser.save();
+  
+      res.status(200).json({
+        message: "user successfully registered!",
+        sevedUser,
+      });
+    } catch (error) {
+      res.status(500).json({
+        message: error.message,
+      });
+    }
+  };
+
+module.exports = {
+    createUser,
+};
