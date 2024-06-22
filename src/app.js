8a63f5f65fae47a3c555e73bf1de979cdb27ffcc
Dariellysantos <daryh_cristina@hotmail.com>
2024-06-21 23:44:31 -0300

    feat: BD configured

--- src/app.js
+++ src/app.js
@@ -1,7 +1,11 @@
 
 require("dotenv").config();
+const db = require("./database/mongoConfig");
+
 
 const express = require("express");
 const app = express();
+
+db.connect();
 
 module.exports = app;
