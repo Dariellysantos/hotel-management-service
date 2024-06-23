0eb0b7de3a63b581f890aa44abd200d78c715639
Dariellysantos <daryh_cristina@hotmail.com>
2024-06-23 13:03:12 -0300

    feat:booking status change route

--- src/controllers/bookingController.js
+++ src/controllers/bookingController.js
@@ -28,41 +28,72 @@ const createBooking = async (req, res) => {
     }
 };
 
+const updateStatus = async (req, res) => {
+    try {
+        const idRequest = req.params.id;
+        let statusRequest = req.body;
+
+        let list = await BookingSchema.find();
+        let found = list.find(i => i._id ==idRequest)
+
+        if (!found) {
+            return res.status(404).json({
+                message: "Booking not found.",
+                code: "NOT_FOUND_ERROR",
+            });
+        }
+
+        found.status = statusRequest.status
+
+        return res.status(200).json({
+                mensagem: "Booking status updated successfully",
+                found,
+            });
+    } catch {
+        return res.status(500).json({
+            message: "Internal error.",
+            code: "INTERNAL_SERVER_ERROR",
+            data: null,
+        });
+    }
+};
+
 const bookingListByDate = async (req, res) => {
     const body = req.body;
 
     try {
-    const booking = await BookingSchema.find();
+        const booking = await BookingSchema.find();
 
-    
-    let list = booking.filter(function (i) {
-        return i.dateOfStay[0] >= body.checkin && i.dateOfStay[1] <= body.checkout
-    })
 
-    if (list.length === 0) {
-        return res.status(404).json({
-            message: "bookings not found.",
-            code: "NOT_FOUND_ERROR",
+        let list = booking.filter(function (i) {
+            return i.dateOfStay[0] >= body.checkin && i.dateOfStay[1] <= body.checkout
+        })
+
+        if (list.length === 0) {
+            return res.status(404).json({
+                message: "bookings not found.",
+                code: "NOT_FOUND_ERROR",
+                data: null,
+            });
+        }
+
+        return res.status(200).json({
+            message: list,
+            code: "SUCCESS",
+        });
+
+    }
+    catch (err) {
+        return res.status(500).json({
+            message: "Internal error.",
+            code: "INTERNAL_SERVER_ERROR",
             data: null,
         });
-    }
-
-    return res.status(200).json({
-        message: list,
-        code: "SUCCESS",
-    });
-
-    } 
-    catch (err) {
-      return res.status(500).json({
-        message: "Internal error.",
-        code: "INTERNAL_SERVER_ERROR",
-        data: null,
-      });
     }
 };
 
 module.exports = {
     createBooking,
     bookingListByDate,
+    updateStatus,
 };
--- src/routes/bookingRouter.js
+++ src/routes/bookingRouter.js
@@ -4,8 +4,8 @@ const router = express.Router();
 const controller = require("../controllers/bookingController");
 
 router.post("/", controller.createBooking);
- router.post("/bookingList", controller.bookingListByDate);
-
+router.post("/bookingList", controller.bookingListByDate);
+router.put("/:id", controller.updateStatus);
 
 
 module.exports = router;
