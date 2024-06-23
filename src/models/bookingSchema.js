28fb2c38d308560f67a350ab20c3bf3f69b8cf0d
Dariellysantos <daryh_cristina@hotmail.com>
2024-06-22 21:22:24 -0300

    feat:schema booking bd configured

---
+++ src/models/bookingSchema.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+
+const bookingSchema = new mongoose.Schema({
+  id: mongoose.Schema.Types.ObjectId,
+  name: {
+    type: String,
+    required: true,
+  },
+  roomNumber: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  amountToPay: {
+    type: Number,
+    required: true,
+  },
+  dateOfStay: {
+    type: Array,
+    required: true,
+  },
+  status: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: new Date(),
+  },
+  updatedAt: {
+    type: Date,
+    default: new Date(),
+  },
+  deletedAt: {
+    type: Date,
+    default: new Date(),
+  },
+});
+
+module.exports = mongoose.model("booking", bookingSchema);
